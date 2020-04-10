c90546833f3c87e35f2a2c03035a4713f35579c9
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-04-10 16:35:11 -0500

    Se añade conversión de datos y se obtiene los parametros para ejecutar la peticion

--- src/main/java/com/sumel/jobsearch/JobSearch.java
+++ src/main/java/com/sumel/jobsearch/JobSearch.java
@@ -2,11 +2,40 @@ package com.sumel.jobsearch;
 
 import com.beust.jcommander.JCommander;
 import com.sumel.jobsearch.cli.CLIArguments;
-import static com.sumel.jobsearch.cli.CommanderFunctions.buidCommanderWithName;
+import com.sumel.jobsearch.model.JobPosition;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.sumel.jobsearch.cli.CommanderFunctions.*;
 
 public class JobSearch {
     public static void main(String[] args) {
         System.out.println("---Hello say Job Search---");
         JCommander jCommander = buidCommanderWithName("job-search", CLIArguments::newInstance);
+        /*Se obtinen en Streams los argumentos que se dieron por erminal*/
+        Stream<CLIArguments> streamOfCLI =
+                parseArguments(jCommander, args, JCommander::usage)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(obj->(CLIArguments)obj);
+
+        /*Se obtiene los argumentos que no sean ayuda y los que sean Keywords*/
+        Optional<CLIArguments> cliArgumentsOptional =
+                streamOfCLI.filter(cli->!cli.isHelp())
+                .filter(cli->cli.getKeyWord() != null)
+                .findFirst();
+
+        /*Convertir el Optional para la URL y se ejecuta la peticion*/
+        cliArgumentsOptional.map(CLIFunctions::toMap)
+                .map(JobSearch::executeRequest)
+                .orElse(Stream.empty())
+                .foreach(System.out::println);
+    }
+
+    private static  Stream<JobPosition> executeRequest(Map<String,Object> params){
+        
     }
 }
--- src/main/java/com/sumel/jobsearch/cli/CommanderFunctions.java
+++ src/main/java/com/sumel/jobsearch/cli/CommanderFunctions.java
@@ -1,7 +1,11 @@
 package com.sumel.jobsearch.cli;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class CommanderFunctions {
@@ -12,5 +16,20 @@ public class CommanderFunctions {
                 .build();
         jCommander.setProgramName(cliName);
         return jCommander;
+    }
+
+    /**Funcion que ayuda a transformar argumentos
+     * En caso de Execption se mostrara la ayuda dada supplier
+     * */
+    public static Optional<List<Object>> parseArguments(JCommander jCommander, String[] args, Consumer<JCommander> onError){
+
+        try {
+            jCommander.parse(args);
+            return  Optional.of(jCommander.getObjects());
+        }catch (ParameterException e){
+            e.printStackTrace();
+            onError.accept(jCommander);
+        }
+        return Optional.empty();
     }
 }
